691d9f43e0f1086112d64c88d7702fdd32886666
Jorge Muñoz Guerrero <131190211+JorgeMunoz1991@users.noreply.github.com>
2023-06-08 18:26:00 +0000

    excuse-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function onLoad() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = Math.floor(Math.random() * who.length);
+  let primero = who[quien];
+
+  let accion = Math.floor(Math.random() * action.length);
+  let segundo = action[accion];
+
+  let que = Math.floor(Math.random() * what.length);
+  let tercero = what[que];
+
+  let cuando = Math.floor(Math.random() * when.length);
+  let cuarto = when[cuando];
+
+  let Array = primero + " " + segundo + " " + tercero + " " + cuarto;
+  document.getElementById("excuse").innerHTML = Array;
 };
